2aa9840481e9f8df4b4267ddc51c49ffec53e3c0
Frank Stallone <frank@designzen.co>
2024-04-30 08:55:52 -0400

    refactor: ♻️  Old Accordion turned to FAQ and new more accessible Accordion component added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,19 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
-
 import tailwind from '@astrojs/tailwind';
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://straightforwardgrowth.com',
-  integrations: [
-    sitemap(),
-    partytown({
-      // Example: Add dataLayer.push as a forwarding-event.
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [sitemap(), partytown({
+    // Example: Add dataLayer.push as a forwarding-event.
+    config: {
+      forward: ['dataLayer.push']
+    }
+  }), tailwind({
+    applyBaseStyles: false
+  }), alpinejs()]
 });
