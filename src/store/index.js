85d1639c846e80efc63fb2c7a5005413311d5c63
Frank Stallone <frank@designzen.co>
2024-06-09 06:09:55 -0400

    refactor: ♻️  Abstracted FAQs

---
+++ src/store/index.js
@@ -0,0 +1,161 @@
+/**
+ * HTML wrapper for Prettier formatting
+ */
+const html = String.raw;
+
+export const faqs = [
+  {
+    title: 'Why do you work with health and wellness businesses, specifically?',
+    description: html`<p>
+      15 years deep into our professional journey, we're acutely aware of how
+      easy it is to slip into an unhealthy lifestyle when you're busy. And,
+      frankly, that story is all too common for people the world over. There are
+      some incredible health and wellness companies out there making real change
+      to people's lives, and we love the idea of dialing our focus into
+      supporting those who are driving that change. We're also firm believers
+      that happy, healthy people create better businesses.
+    </p>`,
+  },
+  {
+    title: 'What does a completed set of research include?',
+    description: html`<p>
+      EVERYTHING you need to write (or have us write) compelling content that
+      your target audience is currently searching for every day. That includes
+      everything from an optimized title, to the key phrases you'll need to
+      incorporate, to example sites that are ranking well in the same space, to
+      specific guidance on how best to tailor the piece. We'll even include
+      suggestions on how to position any relevant CTAs.
+    </p>`,
+  },
+  {
+    title: "What is a 'content asset'?",
+    description: html`<ul class="pl-zero">
+      <li>
+        Completely new idea, or a strategic re-work of content you already have
+        live.
+      </li>
+      <li>Blog post, or landing page long form content</li>
+      <li>Title to be used as the slug or URL</li>
+      <li>Keyphrases to include in the content</li>
+      <li>Example websites that rank highly for these keyphrases</li>
+      <li>Analysis key findings to help you better understand the outcomes</li>
+    </ul>`,
+  },
+  {
+    title: 'Why do content assets come in landing pages or blog posts?',
+    description: html`<p>
+      In a world where distributing content usually happens on services we don't
+      own (think Instagram, Facebook etc.), having a website with your own
+      pages, blog and traffic is the only way to truly own your content assets.
+      What people are actively searching for is best delivered via either a
+      landing page or a blog post. Then, from these long-form content asset
+      landing pages or blog posts, you can distribute multiple short-form
+      content pieces from each and every asset across multiple social channels.
+    </p>`,
+  },
+  {
+    title: "What is this 'Content Machine' framework?",
+    description: html`<p>
+        The Content Machine is a Notion template and a framework designed to
+        make content research, creation, organization, and scheduling
+        straightforward and effective. It's built for small business teams,
+        freelancers and solopreneurs who want to create better content, faster.
+        It will help you:
+      </p>
+      <h3 class="text-step-00">Research and plan with ease</h3>
+      <p>
+        Content Machine gives you a dedicated space to consolidate market
+        research, competitive insights, key phrases for SEO, and creative
+        inspiration, right where you plan and produce your content. This clever
+        integration means you can easily reference your foundational research as
+        you develop every piece of content.
+      </p>
+      <h3 class="text-step-00">Create content that resonates</h3>
+      <p>
+        Draft, edit, and finalize both long and short-form content within a
+        single, unified space. From long-form articles to quick social media
+        posts, Content Machine streamlines the whole process, helping you
+        produce content that is clear, engaging, and always resonant.
+      </p>
+      <h3 class="text-step-00">Organize effortlessly</h3>
+      <p>
+        Say goodbye to scattered documents and disjointed workflows. Content
+        Machine brings harmony to your content strategy, enabling you to link
+        short and long-form pieces, categorize your work, and keep everything in
+        the same place. This smart organization reduces the time from concept to
+        publication, allowing you to focus less on managing and more on
+        creating.
+      </p>
+      <h3 class="text-step-00">Schedule with precision</h3>
+      <p>
+        View your entire content calendar at a glance. Schedule, rearrange, plan
+        ahead, and easily go back in time to find past work that's ripe for
+        repurposing. The flexibility to adapt to new trends or audience needs
+        will keep your content strategy dynamic and relevant. What you publish
+        will always be on time, on brand.
+      </p>
+      <h3 class="text-step-00">Collaborate seamlessly</h3>
+      <p>
+        Whether you're on your own or part of a team, Content Machine
+        facilitates easy collaboration whenever you need it. Share feedback,
+        suggestions, and edits in real time, right from the same space where
+        you're creating and organizing your content. Collaboration, without the
+        chaos.
+      </p>`,
+  },
+  {
+    title: 'Will this help reduce my paid ad spend?',
+    description: html`<p>
+      Almost certainly, yes. The more of a domain authority you become (by
+      search engines recognizing that you create high quality content that your
+      audience wants to read), the more free, organic traffic your site will
+      receive. This takes time, energy, and effort, but with the right content
+      your organic traffic <em>will</em> grow.
+    </p>`,
+  },
+  {
+    title: 'How does my subscription work?',
+    description: html`<p>
+      Subscriptions start immediately after you sign up and automatically renew
+      every 4 weeks. If you need to pause your subscription, let us know and
+      we'll note the days left in your current month. Upon unpausing, you'll
+      still have those remaining days before your subscription renews again.
+      While you're paused, your team's access level to the content machine is
+      moved to comment only. If you decide you no longer need us, you can cancel
+      at any time. Upon cancellation, your team's access level to the content
+      machine is moved to view only. We'll hold everything there for 90 days
+      after cancellation, after which your content machine and set of assets
+      will no longer be available.
+    </p>`,
+  },
+  {
+    title: 'Can I pause or cancel any time?',
+    description: html`<p>
+      Absolutely. Unlike the old agency model, we don't want to burden you with
+      long-term, inflexible contracts and costs. You can switch our service on
+      any time, and pause (or fully cancel) whenever your content needs drop
+      down in importance.
+    </p>`,
+  },
+  {
+    title: 'What about refunds?',
+    description: html`<p>
+        Because our service is month to month, and because we deliver high
+        quality work extremely quickly, we don't provide refunds. But you will
+        only ever be charged for one month in advance, and you can cancel any
+        time.
+      </p>
+      <ul>
+        <li>How does the subscription work?</li>
+        <li>Renews every 4 weeks</li>
+        <li>
+          Pausing means we keep your Content Machine on hold from adding
+          anything new but you can still use it
+        </li>
+        <li>
+          Canceling means you need to duplicate the Content Machine to your own
+          Notion account
+        </li>
+      </ul>`,
+  },
+];
