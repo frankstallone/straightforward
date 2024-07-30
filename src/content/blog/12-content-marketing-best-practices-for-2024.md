83c03feb995078ce3a6bf75ffefa04e4face4691
Frank Stallone <frank@designzen.co>
2024-07-30 10:50:00 -0400

    wip: 🚧 Custom blog hero images roughed in

---
+++ src/content/blog/12-content-marketing-best-practices-for-2024.md
@@ -0,0 +1,10 @@
+---
+isDraft: false
+title: '12 Content Marketing Best Practices For 2024'
+publishDate: 07-30-2024
+description: 'TBD'
+heroImage: '../../assets/deep-connection-health-and-wellness-content-marketing.jpg'
+alt: 'Man running listening to a podcast'
+---
+
+## Test
--- src/content/config.ts
+++ src/content/config.ts
@@ -3,18 +3,25 @@ import { z, defineCollection } from 'astro:content';
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
   type: 'content',
-  schema: z.object({
-    isDraft: z.boolean(),
-    title: z.string(),
-    description: z.string(),
-    subtitle: z.string().optional(),
-    // In frontmatter, dates written without quotes around them are interpreted as Date objects
-    publishDate: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-    image: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      isDraft: z.boolean(),
+      title: z.string(),
+      description: z.string(),
+      subtitle: z.string().optional(),
+      // In frontmatter, dates written without quotes around them are interpreted as Date objects
+      publishDate: z
+        .string()
+        .or(z.date())
+        .transform((val) => new Date(val)),
+      heroImage: image()
+        .refine((img) => img.width >= 1080, {
+          message: 'Cover image must be at least 1080 pixels wide!',
+        })
+        .optional(),
+      alt: z.string().optional(),
+      socialImage: z.string().optional(),
+    }),
 });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
